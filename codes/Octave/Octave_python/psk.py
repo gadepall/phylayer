0a584a8dc996bc746c4be9f9f343cd6b09510ab0
gadepall <gadepall@gmail.com>
2020-08-25 18:26:15 +0530

    Add existing files

---
+++ codes/Octave/Octave_python/psk.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+
+x = np.loadtxt('psk.dat',dtype='double')
+
+snr_db = np.vstack((x[0,:]))
+ser_ana = np.vstack((x[1,:]))
+ser = np.vstack((x[2,:]))
+ber = np.vstack((x[3,:]))
+print(ser_ana)
+
+plt.grid(True, which = "both")
+plt.semilogy(snr_db, ser_ana, label='SER Analysis')
+plt.semilogy(snr_db, ser, 'o', label = 'SER Simulation')
+plt.semilogy(snr_db, ber, label = 'BER Simulation')
+plt.xlabel('SNR in dB')
+plt.ylabel('Error Rate')
+plt.legend()
+plt.title('BER & SER curve for 8PSK')
+
+#if using termux
+plt.savefig('./figs/psk.pdf')
+plt.savefig('./figs/psk.eps')
+
+#plt.show()
+subprocess.run(shlex.split('termux-open ./figs/psk.pdf'))
